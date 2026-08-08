09e448f8096e0ed98057be7b8401926175c5db2c
thomasdar-lgtm <thomas.darregert@gmail.com>
2026-08-08 17:55:11 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '2.32';
+const CACHE_VERSION = '2.33';
 const CACHE_NAME = 'pokoalashop-v' + CACHE_VERSION;
 const ASSETS = ['./', './index.html', './manifest.json', './icons/logo.png', './icons/icon-192.png', './icons/icon-512.png'];
 
